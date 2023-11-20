23f9784f1217a7f946e990f14f093f09e2de22b3
Elena-admin <avb.moons@gmail.com>
2023-11-20 22:23:02 +0300

    add components

---
+++ scripts/menuFuncs.js
@@ -0,0 +1,36 @@
+function toDatabase() {
+  window.location.href = "pages/database.html";
+}
+
+function toStatistic() {
+  window.location.href = "pages/statistic.html";
+}
+
+function toLogs() {
+  window.location.href = "pages/logs.html";
+}
+
+function toUsers() {
+  window.location.href = "pages/users.html";
+}
+
+////////////////////////////
+
+// function toDatabasePages() {
+//   window.open("../pages/database.html", "_blank");
+// }
+function toDatabasePages() {
+  window.location.href = "../pages/database.html";
+}
+
+function toStatisticPages() {
+  window.location.href = "../pages/statistic.html";
+}
+
+function toLogsPages() {
+  window.location.href = "../pages/logs.html";
+}
+
+function toUsersPages() {
+  window.location.href = "../pages/users.html";
+}
